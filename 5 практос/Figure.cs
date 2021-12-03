0db5874745441c920b2696cef73f3367d31160b3
исп-43 <-43@LAB24-13>
2021-12-03 13:58:45 +0300

    класс figure

--- 5 практос/Figure.cs
+++ 5 практос/Figure.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace _5_практос
+namespace praktika
 {
-    class Figure
+    public abstract class Figure
     {
+        //Создаём начальные точки
+        public int X { get; set; }
+        public int Y { get; set; }
+        public abstract string Draw();
+
+        //Создаём канструктор чтоб в Х и У загрузить значения
+        public Figure(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
     }
 }
